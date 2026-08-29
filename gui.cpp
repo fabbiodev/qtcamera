9b2b1aa13f5a1c6b3b739942873e1f05d6d7812e
fabbiodev <fabbiodev@users.noreply.github.com>
2026-08-29 14:25:26 +0300

    Revert "Use buffered painter widget for stable frame display"
    This reverts commit dffbce63934389bea451c91b86a2210263ade901.

--- gui.cpp
+++ gui.cpp
@@ -1,63 +1,25 @@
 #include "gui.h"
 
 #include <QLabel>
-#include <QPainter>
+#include <QPixmap>
 #include <QVBoxLayout>
 #include <QWidget>
 
 namespace {
-
-class FrameWidget final : public QWidget
-{
-public:
-    explicit FrameWidget(QWidget *parent = nullptr) : QWidget(parent)
-    {
-        setMinimumSize(512, 384);
-        setAttribute(Qt::WA_OpaquePaintEvent);
-        setAutoFillBackground(false);
-    }
-
-    void setFrame(const QImage &image)
-    {
-        frame_ = image.copy();
-        update();
-    }
-
-protected:
-    void paintEvent(QPaintEvent *) override
-    {
-        QPainter painter(this);
-        painter.fillRect(rect(), Qt::black);
-        if (frame_.isNull()) {
-            return;
-        }
-
-        const QSize targetSize = frame_.size().scaled(size(), Qt::KeepAspectRatio);
-        const QRect target((width() - targetSize.width()) / 2,
-                           (height() - targetSize.height()) / 2,
-                           targetSize.width(),
-                           targetSize.height());
-        painter.setRenderHint(QPainter::SmoothPixmapTransform, false);
-        painter.drawImage(target, frame_);
-    }
-
-private:
-    QImage frame_;
-};
-
 QWidget *window_ = nullptr;
 QLabel *status_ = nullptr;
-FrameWidget *frame_ = nullptr;
+QLabel *frame_ = nullptr;
 QLabel *info_ = nullptr;
-
-} // namespace
+}
 
 void guiCreate()
 {
     window_ = new QWidget;
     status_ = new QLabel(QStringLiteral("Camera: starting..."), window_);
-    frame_ = new FrameWidget(window_);
+    frame_ = new QLabel(QStringLiteral("No frame received"), window_);
     info_ = new QLabel(window_);
+    frame_->setMinimumSize(512, 384);
+    frame_->setAlignment(Qt::AlignCenter);
 
     auto *layout = new QVBoxLayout(window_);
     layout->addWidget(status_);
@@ -76,7 +38,8 @@ void guiSetCameraStatus(const QString &text)
 
 void guiShowFrame(const QImage &image, std::size_t bytes, std::size_t number)
 {
-    frame_->setFrame(image);
+    frame_->setPixmap(QPixmap::fromImage(image).scaled(
+        frame_->size(), Qt::KeepAspectRatio, Qt::FastTransformation));
     info_->setText(QStringLiteral("Frame %1, %2 RAW bytes")
                       .arg(static_cast<qulonglong>(number))
                       .arg(static_cast<qulonglong>(bytes)));
